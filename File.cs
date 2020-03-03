21b4a4435c3ace81f235f9889fe05636fc94aea3
Charlie N A <lambopuggy@gmail.com>
2020-03-03 09:42:52 +0000

    Add project files.

---
+++ File.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment_2_Code_Review
+{
+    class Txt_File
+    {
+        List<string> file;
+        public int length;
+        Dictionary<string, int> compressedFile;
+
+        public Txt_File(List<string> f)
+        {
+            file = f;
+            length = f.Count;
+        }
+        public Dictionary<string, int> Get_Dictionary()
+        {
+            foreach (string word in file)
+            {
+                if (compressedFile.ContainsKey(word))
+                {
+                    compressedFile[word] += 1;
+                }
+                else
+                {
+                    compressedFile.Add(word, 1);
+                }
+            }
+            return compressedFile;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Assignment_2_Code_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
