035888d149d6c261b987a4709fd4a14b2116e54e
Charlie N A <lambopuggy@gmail.com>
2020-03-03 11:59:37 +0000

    bruh

--- Program.cs
+++ Program.cs
@@ -14,13 +14,13 @@ namespace OOP_Assignment_2_Code_Review
             DirectoryInfo d = new DirectoryInfo(Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents"));
             FileInfo[] Files = d.GetFiles("*.txt");
             FileInfo filechoice1 = Files[0];
-            int count = 1,UserFileChoice1 = 0, UserFileChoice2 =0;
+            int count = 1, UserFileChoice1 = 0, UserFileChoice2 = 0;
             bool leave = false, failed = false;
             Console.WriteLine("Good Morning, I am in your documents folder, what txt files would you like to compare ");
             foreach (FileInfo txts in Files)
             {
-                string[] justName= txts.ToString().Split("\\");
-                Console.Write($"{count}.{justName[justName.Length-1]}, ");
+                string[] justName = txts.ToString().Split("\\");
+                Console.Write($"{count}.{justName[justName.Length - 1]}, ");
                 count++;
             }
             Console.WriteLine("\n Please enter the number");
@@ -41,24 +41,24 @@ namespace OOP_Assignment_2_Code_Review
                 }
             }
 
-            List<string> FirstFile = new List<string> (File.ReadAllLines(Files[UserFileChoice1 - 1].ToString()));
-            List<string> SecondFile = new List<string> (File.ReadAllLines(Files[UserFileChoice2 - 1].ToString()));
+            List<string> FirstFile = new List<string>(File.ReadAllLines(Files[UserFileChoice1 - 1].ToString()));
+            List<string> SecondFile = new List<string>(File.ReadAllLines(Files[UserFileChoice2 - 1].ToString()));
 
 
             Console.WriteLine("Do you want the filter to be case sensitive? (Y/N)");
             leave = false;
-            while(!leave)
+            while (!leave)
             {
                 string caseSense = Console.ReadLine();
                 if (caseSense.ToLower().Trim() == "y" || caseSense.ToLower().Trim() == "n")
                 {
                     leave = true;
-                    if(caseSense.ToLower().Trim() == "y")
+                    if (caseSense.ToLower().Trim() == "y")
                     {
                         FirstFile = FirstFile.Select(x => x.ToLower()).ToList();
                         SecondFile = SecondFile.Select(x => x.ToLower()).ToList();
                     }
-                    
+
                 }
 
                 else
@@ -66,7 +66,7 @@ namespace OOP_Assignment_2_Code_Review
             }
             Txt_File First = new Txt_File(FirstFile);
             Txt_File Second = new Txt_File(SecondFile);
-            List<(string,int)> SecList= Second.Get_List(),FirList = First.Get_List();
+            List<(string, int)> SecList = Second.Get_List(), FirList = First.Get_List();
             try
             {
                 for (int i = 0; i <= SecList.Count(); i++)
@@ -86,19 +86,7 @@ namespace OOP_Assignment_2_Code_Review
             {
                 Console.WriteLine("the files are not the same");
             }
-            
+
         }
-
-
-
-
-
-
-
-
-
-
-
-    }
     }
 }
